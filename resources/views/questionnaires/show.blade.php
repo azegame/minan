6f4406ffa7f8b594eef59f25a26817ef8ddad8e8
azegame <a.masashi218@gmail.com>
2024-01-20 00:45:25 +0900

    edit tailwindCSS

--- resources/views/questionnaires/show.blade.php
+++ resources/views/questionnaires/show.blade.php
@@ -1,21 +1,23 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投票画面</title>
-</head>
-
-<body>
-    <div>
-        選択肢
-        @foreach($options as $option)
-        <div>
-            {{ $option->option_name}}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            投票画面
+        </h2>
+    </x-slot>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+                @foreach($options as $option)
+                <div class="sm:w-1/2 mb-10 px-4">
+                    <div class="rounded-lg h-64 overflow-hidden">
+                        <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1201x501">
+                    </div>
+                    <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">{{ $option->option_name}}</h2>
+                    <p class="leading-relaxed text-base">{{ $option->option_name}}</p>
+                    <button class="flex mx-auto mt-6 text-white bg-blue-500 border-0 py-2 px-5 focus:outline-none hover:bg-blue-600 rounded">投票</button>
+                </div>
+                @endforeach
+            </div>
         </div>
-        @endforeach
-    </div>
-</body>
-
-</html>
+    </section>
+</x-app-layout>
