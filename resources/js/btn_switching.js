1e3c8a9cff3b526d484aa0c9a72efa8461250f0b
azegame <a.masashi218@gmail.com>
2024-01-30 00:16:06 +0900

    投票機能進んだ

--- resources/js/btn_switching.js
+++ resources/js/btn_switching.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let hasVoted = document.body.getAttribute('data-has-voted') === 'true';
+    // let hasVoted = document.body.getAttribute('data-has-voted') === 'true';
+    
     const radioBtns = document.querySelectorAll('.switch_btn');
     let voteButton = document.querySelector('.vote-button');
+    let hasVoted = voteButton.getAttribute('data-has-voted') === 'true';
+    console.log(hasVoted);
     let previousOptionId = null;
 
     if (hasVoted) {
+        console.log('再投票');
+        voteButton.textContent = '再投票';
+        voteButton.disabled = true;
         radioBtns.forEach(radioBtn => {
             if (radioBtn.checked) {
                 previousOptionId = radioBtn.getAttribute('data-option-id').trim();
                 console.log(previousOptionId);
             }
          });
-    
-        voteButton.textContent = '再投票';
-        voteButton.disabled = true;
-        
         radioBtns.forEach(radioBtn => {
             radioBtn.addEventListener('change', function() {
                 if (radioBtn.checked) {
@@ -32,18 +34,21 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         // 未投票の場合
     } else {
+        console.log('初めての投票');
         voteButton.textContent = '投票';
         voteButton.disabled = true;
+        voteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
+        voteButton.classList.add('bg-green-200', 'hover:bg-green-200');
         radioBtns.forEach(radioBtn => {
             radioBtn.addEventListener('change', function() {
                 if (radioBtn.checked) {
                     voteButton.disabled = false;
-                    voteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
-                    voteButton.classList.add('bg-green-200', 'hover:bg-green-200');
-                } else {
-                    voteButton.disabled = false;
                     voteButton.classList.remove('bg-green-200', 'hover:bg-green-200');
                     voteButton.classList.add('bg-green-500', 'hover:bg-green-600');
+                } else {
+                    voteButton.disabled = true;
+                    voteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
+                    voteButton.classList.add('bg-green-200', 'hover:bg-green-200');
                 }
             });
             });
