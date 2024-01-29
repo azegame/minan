dcaea3fc31ae427d14463edb52cbb8ba340767e8
azegame <a.masashi218@gmail.com>
2024-01-29 23:58:51 +0900

    投票機能進んだ

--- resources/js/btn_switching.js
+++ resources/js/btn_switching.js
@@ -1,35 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // let hasVoted = document.body.getAttribute('data-has-voted') === 'true';
+    let hasVoted = document.body.getAttribute('data-has-voted') === 'true';
     const radioBtns = document.querySelectorAll('.switch_btn');
     let voteButton = document.querySelector('.vote-button');
     let previousOptionId = null;
 
-    radioBtns.forEach(radioBtn => {
-        if (radioBtn.checked) {
-            previousOptionId = radioBtn.getAttribute('data-option-id').trim();
-            console.log(previousOptionId);
-        }
-     });
-
-    radioBtns.forEach(radioBtn => {
-        radioBtn.addEventListener('change', function() {
+    if (hasVoted) {
+        radioBtns.forEach(radioBtn => {
             if (radioBtn.checked) {
-                let currentOptionId = radioBtn.getAttribute('data-option-id').trim();
-                console.log(currentOptionId);
-                if (currentOptionId === previousOptionId) {
-                    voteButton.disabled = true;
-                    voteButton.textContent = '再投票';
+                previousOptionId = radioBtn.getAttribute('data-option-id').trim();
+                console.log(previousOptionId);
+            }
+         });
+    
+        voteButton.textContent = '再投票';
+        voteButton.disabled = true;
+        
+        radioBtns.forEach(radioBtn => {
+            radioBtn.addEventListener('change', function() {
+                if (radioBtn.checked) {
+                    let currentOptionId = radioBtn.getAttribute('data-option-id').trim();
+                    if (currentOptionId === previousOptionId) {
+                        voteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
+                        voteButton.classList.add('bg-green-200', 'hover:bg-green-200');
+                    } else {
+                        voteButton.disabled = false;
+                        voteButton.classList.remove('bg-green-200', 'hover:bg-green-200');
+                        voteButton.classList.add('bg-green-500', 'hover:bg-green-600');
+                    }
+                }
+            });
+        });
+        // 未投票の場合
+    } else {
+        voteButton.textContent = '投票';
+        voteButton.disabled = true;
+        radioBtns.forEach(radioBtn => {
+            radioBtn.addEventListener('change', function() {
+                if (radioBtn.checked) {
+                    voteButton.disabled = false;
                     voteButton.classList.remove('bg-green-500', 'hover:bg-green-600');
                     voteButton.classList.add('bg-green-200', 'hover:bg-green-200');
                 } else {
                     voteButton.disabled = false;
-                    voteButton.textContent = '再投票';
                     voteButton.classList.remove('bg-green-200', 'hover:bg-green-200');
                     voteButton.classList.add('bg-green-500', 'hover:bg-green-600');
                 }
-            } else {
-                voteButton.disabled = true;
-            }
-        });
-    });
+            });
+            });
+    }
 });
